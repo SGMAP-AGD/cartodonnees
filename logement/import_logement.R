2c4d4873e18fc0fec7f5e4cb1e04a85e91b39e0b
paul <paul.bouchequet@gmail.com>
2016-02-05 14:32:49 +0100

    erreur hash corrigée

--- logement/import_logement.R
+++ logement/import_logement.R
@@ -1,2 +1,48 @@
 df <- read.csv("https://www.data.gouv.fr/s/resources/ouverture-des-donnees-publiques-du-logement-rapport-et-cartographie/20151002-171050/Cartographie_des_donnees_logement_-_2015-01-26.csv")
 
+by(df, 1:nrow(df), function(row){
+  
+  row <- as.list(row)
+  
+  #"Bases.enquêtes.données"     
+  row['nom'] <- row['Bases.enquêtes.données']
+  row['Bases.enquêtes.données'] <- NULL
+  
+  #"Gestionnaire"   
+  row['gestionnaire'] <- row['Gestionnaire']
+  row['Gestionnaire'] <- NULL
+  
+  #"Description"
+  row['description'] <- row['Description']
+  row['Description'] <- NULL
+  
+  #"Origine.des.données"
+  row['origine'] <- row['Origine.des.données']
+  row['Origine.des.données'] <- NULL
+  
+  #"Conditions.d.accès"
+  
+  #"Coût.d.accès"
+  row['Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement'] <- NULL
+  
+  #"Données.en.open.data"
+  row['Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement'] <- NULL
+  
+  #"Granularité.géographique"
+
+  
+  #"Fréquence"
+  row['Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement'] <- NULL
+  
+  #"Qualité.des.données"  
+  row['Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement'] <- NULL
+  
+  #"Remarques.et.perspectives.éventuelles"
+  row['commentaire'] <- row['Remarques.et.perspectives.éventuelles']
+  row['Remarques.et.perspectives.éventuelles'] <- NULL
+  
+  #"Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement"
+  row['Pour.en.savoir.plus..fiche.de.l.annexe.12.du.rapport.sur.l.organisation.du.service.statistique.dans.le.domaine.du.logement'] <- NULL
+  
+  write(toJSON(row), paste("data/",digest(paste(row['gestionnaire'],row['nom']),algo="md5"),".json",sep=""))
+})
