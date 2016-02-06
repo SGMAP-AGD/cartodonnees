7b8b4e2c0d5fa67ee7ec80c6b5da5df10ad1e7f9
paul <paul.bouchequet@gmail.com>
2016-02-06 10:47:59 +0100

    -

---
+++ visualisation.R
@@ -0,0 +1,14 @@
+# https://briatte.github.io/ggnet/
+
+library(GGally)
+library(network)
+library(sna)
+library(ggplot2)
+
+net = rgraph(10, mode = "graph", tprob = 0.5)
+net = network(net, directed = FALSE)
+network.vertex.names(net) = letters[1:10]
+
+ggnet2(net)
+
+?rgraph
